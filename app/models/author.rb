41a95702663dec7a0f7c32800a16f70b831b49cc
rebecca-eakins <rebecca.a.eakins@gmail.com>
2015-05-07 12:58:28 -0400

    complete task 3 - author name formatted on post pages & new post simple form

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,4 +6,8 @@ class Author < ActiveRecord::Base
   validates :email_address, presence: true,
     format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
               message: 'only valid e-mail addresses allowed' }
+
+  def formatted_name
+    "#{first_name} #{last_name}"
+  end
 end
